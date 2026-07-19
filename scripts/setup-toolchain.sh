a91c581ab728835afc5b236773bb037dcbe580da
fwlta <farmaram@proton.me>
2026-07-19 15:26:09 +0000

    switch to Neutron Clang 30062026

--- scripts/setup-toolchain.sh
+++ scripts/setup-toolchain.sh
@@ -4,14 +4,14 @@ set -e
 
 : "${WORK_DIR:=$GITHUB_WORKSPACE}"
 
-CLANG_VER="ZyC-Clang-23.0.0git"
-CLANG_URL="https://github.com/yurika-sudo/clang-repo/releases/download/clang-1/${CLANG_VER}.tar.gz"
+CLANG_VER="neutron-clang-30062026"
+CLANG_URL="https://github.com/Neutron-Toolchains/clang-build-catalogue/releases/download/30062026/${CLANG_VER}.tar.zst"
 CLANG_DIR="${WORK_DIR}/prebuilts/clang/host/linux-x86/clang-${CLANG_VER}"
 
 mkdir -p "$CLANG_DIR"
-aria2c -x16 -s16 -d /tmp -o clang.tar.gz "$CLANG_URL"
-tar -xzf /tmp/clang.tar.gz -C "$CLANG_DIR"
-rm -f /tmp/clang.tar.gz
+aria2c -x16 -s16 -d /tmp -o clang.tar.zst "$CLANG_URL"
+tar --zstd -xf /tmp/clang.tar.zst -C "$CLANG_DIR"
+rm -f /tmp/clang.tar.zst
 
 echo "CLANG_DIR=$CLANG_DIR" >> "${GITHUB_ENV:-/dev/null}"
 echo "[OK] Toolchain ready: $CLANG_DIR"
